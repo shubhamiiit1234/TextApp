6e87b9e4f6fbecf5df035a4011c3f9c8af3e899a
shubhamiiit1234 <shubhamiiit1234@gmail.com>
2022-06-06 23:22:15 +0530

    Updates

---
+++ precache-manifest.7763756a345be23520248fc182d0a519.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "31863ebe6f93f55778cc",
+    "url": "/TextApp/static/css/main.bde01b32.chunk.css"
+  },
+  {
+    "revision": "31863ebe6f93f55778cc",
+    "url": "/TextApp/static/js/main.61b94759.chunk.js"
+  },
+  {
+    "revision": "f238489a4c5cb7842e10",
+    "url": "/TextApp/static/js/runtime~main.42bbd475.js"
+  },
+  {
+    "revision": "198af7d06ea38f6a66cb",
+    "url": "/TextApp/static/js/2.b7375f94.chunk.js"
+  },
+  {
+    "revision": "2ded26446bc94b81347a",
+    "url": "/TextApp/static/js/3.398cb210.chunk.js"
+  },
+  {
+    "revision": "72152045a108e726a328cf0c04b737b3",
+    "url": "/TextApp/index.html"
+  }
+];
